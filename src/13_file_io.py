d8920b2676b26654f8863680c9f9a34db1dad9ad
BuildTools <arthurpisakhov1@gmail.com>
2019-11-16 00:24:22 -0500

    scopes, file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,25 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('src/foo.txt') as f:
+  read_data = f.read()
+  print(read_data)
+  f.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+f = open('src/bar.txt', 'w')
+f.write('This is a test\n')
+f.write('This is a another test\n')
+f.write('This is a final test\n')
+f.close()
+
+with open('src/bar.txt') as f:
+  read_data = f.read()
+  print(read_data)
+  f.close()
